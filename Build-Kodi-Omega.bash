fda02f6cf584d640b246c1eb8a2a89394d6bc71e
Yusuf.Uzunyilmaz <46284579+Tibsun75@users.noreply.github.com>
2024-12-11 02:07:46 +0100

    Update Build-Kodi-Omega.bash
    added comments and echo commands

--- Build-Kodi-Omega.bash
+++ Build-Kodi-Omega.bash
@@ -1,8 +1,22 @@
+# clone kodi-repository
+echo "Cloning Kodi repository..."
 cd ~
 git clone -b Omega https://github.com/xbmc/xbmc kodi
 cd kodi
+
+# configure with CMake
+echo "Configuring the build with CMake..."
 cmake ../kodi -DCMAKE_INSTALL_PREFIX=/usr/local -DCORE_PLATFORM_NAME=x11 -DAPP_RENDER_SYSTEM=gl -DENABLE_INTERNAL_FLATBUFFERS=ON
+
+# assemble
+echo "Building Kodi..."
 cmake --build . -- VERBOSE=1 -j$(getconf _NPROCESSORS_ONLN)
+
+# install and crate .deb package
+echo "Installing Kodi..."
 sudo make install
 sudo checkinstall
+
+# compile and install binary-addons
+echo "Building and installing binary-addons..."
 sudo make -j$(getconf _NPROCESSORS_ONLN) -C tools/depends/target/binary-addons PREFIX=/usr/local
